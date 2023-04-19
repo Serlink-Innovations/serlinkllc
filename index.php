325faf472ad1283ad2029557de32b1b832742018
Serlink-Innovations <serlink.business@outlook.com>
2023-04-19 15:19:58 -0400

    v1.4

--- index.php
+++ index.php
@@ -16,92 +16,118 @@
 
         <header class="outermost-headerContainer">
 
-            <nav class="navigationBarcontainer container" role="navigation">
+            <div class="outermost-headerContainerOverlay">
 
-                <div class="navigationBar-innerContainer">
+                <nav class="navigationBarcontainer container" role="navigation">
 
-                    <div class="navigationBar-brandContainer">
+                    <div class="navigationBar-innerContainer">
 
-                        <picture class="brand">
-                            <img src="/assets/img/logo-w-trans.png" type="">
-                        </picture>
+                        <div class="navigationBar-brandContainer">
 
-                    </div>
+                            <picture class="brand">
+                                <img src="/assets/img/logo-w-trans.png" type="">
+                            </picture>
 
-                    <div class="navigationBar-navigationItemsContainer">
+                        </div>
 
-                        <ul class="navbar">
+                        <div class="navigationBar-navigationItemsContainer">
 
-                            <li class="nav-items">
+                            <ul class="navbar">
 
-                                <a href="#">
-                                    About
-                                </a>
+                                <li class="nav-items">
 
-                            </li>
+                                    <a href="#">
+                                        About
+                                    </a>
 
-                            <li class="nav-items">
+                                </li>
 
-                                <a href="#">
-                                    Services
-                                </a>
+                                <li class="nav-items">
 
-                            </li>
+                                    <a href="#">
+                                        Services
+                                    </a>
 
-                            <li class="nav-items">
+                                </li>
 
-                                <a href="#">
-                                    Projects
-                                </a>
+                                <li class="nav-items">
 
-                            </li>
+                                    <a href="#">
+                                        Projects
+                                    </a>
 
-                            <li class="nav-items">
+                                </li>
 
-                                <a href="#">
-                                    Team
-                                </a>
+                                <li class="nav-items">
 
-                            </li>
+                                    <a href="#">
+                                        Team
+                                    </a>
 
-                        </ul>
+                                </li>
 
-                    </div>
+                            </ul>
 
-                </div>
-
-            </nav>
-
-            <div class="hero container">
-
-                <div class="hero-innterContentContainer">
-
-                    <h1>
-                        Connecting You to
-                    </h1>
-
-                    <h2>the World</h2>
-
-                    <p>
-                        We specialize in reliable and high-quality IT solutions. From custom software development and
-                        network design to system maintenance and support, we have the expertise to meet all your IT
-                        needs.
-                    </p>
-
-                    <div class="hero-buttonContainer">
-
-                        <button class="btn btn-primary-outlined">
-                            Our Projects
-                        </button>
-
-                        <button class="btn btn-primary">
-                            Contact Us
-                        </button>
+                        </div>
 
                     </div>
 
-                    <div class="hero-clientsLogoContainer">
+                </nav>
 
+                <div class="hero container">
+
+                    <div class="hero-innterContentContainer">
+
+                        <h1>
+                            Connecting You to
+                        </h1>
+
+                        <h2>the World</h2>
+
+                        <p>
+                            We specialize in reliable and high-quality IT solutions. From custom software development
+                            and
+                            network design to system maintenance and support, we have the expertise to meet all your IT
+                            needs.
+                        </p>
+
+                        <div class="hero-buttonContainer">
+
+                            <button class="btn btn-primary-outlined">
+                                Our Projects
+                            </button>
+
+                            <button class="btn btn-primary">
+                                Contact Us
+                            </button>
+
+                        </div>
+
+                        <div class="hero-clientsLogoContainer">
+
+                            <label id="clients" for="">Some of our clients</label>
+
+                            <div id="clients-logoContainer">
+
+                                <a href="https://www.domegencreative.com">
+                                    <svg version="1.0" viewBox="0 0 249 97" xmlns="http://www.w3.org/2000/svg">
+                                        <g transform="translate(0 97) scale(.1 -.1)">
+                                            <path
+                                                d="m736 895c-4-16-9-35-11-42-3-7-24-3-67 15-60 25-73 27-273 30-137 2-207 0-200-6 6-6 19-12 30-14 19-3 20-12 23-276l2-273-32-14c-31-13-23-14 112-17 182-4 268 6 336 38 64 30 127 98 148 161s20 165-4 233c-10 30-17 56-16 58 2 1 23 6 47 11 43 9 43 9 9 10-40 1-65 14-56 29 3 5-3 12-13 15-13 3-21 16-23 38l-4 34-8-30zm-176-34c14-9 36-39 50-67 21-42 25-65 29-168 6-194-34-295-124-312-21-4-50-3-64 2l-26 9-3 255c-1 140 0 265 3 277s13 24 23 26c28 8 85-3 112-22z">
+                                            </path>
+                                            <path
+                                                d="m1546 675c-9-13-16-32-16-42 0-13 6-11 30 12 17 16 30 35 30 42 0 22-27 15-44-12z">
+                                            </path>
+                                            <path
+                                                d="m941 604c-12-15-21-30-21-34 0-5-15-17-34-28-34-20-76-91-76-129 0-11 9-32 21-47 32-40 78-36 125 12 20 20 43 47 51 60 9 12 23 22 33 22 14 0 18-8 17-36-1-31 13-53 33-54 3 0 21 30 39 68 37 76 72 132 84 132 4 0 0-17-9-37-22-51-34-102-27-119 9-24 29-15 51 24 12 20 33 51 47 67l26 30-17-40c-22-52-29-117-15-144 19-36 54-26 99 27l41 47-12-39c-10-36-10-40 16-62 16-13 35-24 44-24s38 23 64 51l47 51 7-30c15-70 63-65 111 12 35 56 46 59 31 9-6-23-33-62-68-98-69-73-109-131-109-159 0-24 27-60 51-69 42-14 94 70 135 216 17 61 25 73 73 113 29 24 57 44 61 44 8 0 7-43 0-65-7-19 11-52 35-65 34-18 66-1 112 63 25 34 39 45 46 38 20-20 35-12 60 32 28 48 71 92 49 50-7-12-12-42-12-67 0-35 4-48 20-56 31-17 52-5 93 50 52 69 46 79-15 26-29-25-56-46-61-46-15 0 4 63 30 103 27 42 27 80 2 85-15 3-43-27-114-123-14-19-14-18-9 15 4 19 15 50 25 68 25 43 24 54-3 50-19-2-28-17-48-77-20-59-36-86-80-133-71-74-84-75-78-6 3 29 10 54 16 56s29 16 51 33c64 46 67 110 6 117-32 4-68-26-96-80-11-21-23-38-27-38-5 0-27-16-51-36-23-20-44-34-47-31-4 3 4 24 16 46 25 43 21 71-8 71-13 0-38-29-76-85-31-47-61-85-66-85-20 0-9 48 25 106 42 69 68 94 101 94 31 0 33 35 2 43-48 13-84-18-217-191-31-40-87-79-98-68-10 10 9 106 22 106 18 0 93 72 100 96 7 27-18 54-48 54-35 0-65-27-93-83-26-51-102-147-116-147s-8 30 18 91c26 63 29 119 6 127-13 4-65-44-94-88-17-24-17-23-3 19 19 56 19 87-1 95-22 8-67-42-101-114-15-33-31-59-35-59-8-1 11 59 24 76 9 11-5 53-17 53-3 0-12-15-21-32-23-46-61-68-50-30 19 67 17 81-8 107-32 32-59 32-85-1zm64-51c-9-65-14-73-41-73-19 0-24 5-24 24 0 32 14 69 35 90 26 25 38 9 30-41zm499-54c-15-16-29-27-31-25-6 6 18 60 35 80 17 18 17 18 20-4 2-14-7-33-24-51zm496 48c0-21-46-77-63-77-4 0 4 23 18 50 27 54 45 64 45 27zm-1030-100c0-14-65-76-85-79-28-4-30 28-6 85l20 47 35-23c19-12 35-25 36-30zm685-238c-31-78-51-109-70-109-18 0-20 42-4 73 20 37 93 120 97 109 1-5-8-38-23-73z">
+                                            </path>
+                                        </g>
+                                    </svg>
+                                </a>
+
+                            </div>
+
+                        </div>
 
                     </div>
 
@@ -110,6 +136,66 @@
             </div>
 
         </header>
+
+        <section class="about-contentContainer">
+
+            <div class="container">
+
+                <h2>About Us</h2>
+
+                <div class="about-contentContainerRow">
+
+                    <div class="about-contentContainerColumn">
+
+                        <p>
+                            At Serlink Innovations, our mission is to provide top-quality IT solutions and services to
+                            help
+                            businesses succeed. We are committed to delivering exceptional results for our clients and
+                            building trust-based, long-term relationships.
+                        </p>
+
+                        <ul>
+
+                            <li>
+                                Founded on the belief that every business should have access to top-quality IT services
+                                and
+                                solutions.
+                            </li>
+
+                            <li>
+                                Team of skilled and experienced professionals with expertise in custom software
+                                development,
+                                network design, system maintenance, and support.
+                            </li>
+
+                            <li>
+                                Committed to delivering results that exceed clients' expectations to ensure utmost
+                                statisfaction.
+                            </li>
+
+                        </ul>
+
+                    </div>
+
+                    <div class="about-contentContainerColumn">
+
+                        <p>
+                            Our team of highly qualified professionals is dedicated to delivering top-quality solutions
+                            and services and exceeding client expectations. We strive to foster enduring partnerships
+                            with our clients.
+                        </p>
+
+                        <button class="btn btn-primary">
+                            More Info
+                        </button>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
 
     </body>
 
