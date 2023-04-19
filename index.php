8f032ed121a6612072e1ce02270b405675c958aa
Serlink-Innovations <serlink.business@outlook.com>
2023-04-19 10:55:48 -0400

    v1.0

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Serlink Innovations LLC</title>
+
+        <link rel="stylesheet" href="assets/css/style.css">
+
+    </head>
+
+    <body>
+
+        <header class="outermost-headerContainer">
+
+            <nav class="navigationBarcontainer container" role="navigation">
+
+                <div class="navigationBar-innerContainer">
+
+                    <div class="navigationBar-brandContainer">
+
+                        <picture class="brand">
+                            <img src="/assets/img/logo-w-trans.png" type="">
+                        </picture>
+
+                    </div>
+
+                    <div class="navigationBar-navigationItemsContainer">
+
+                        <ul class="navbar">
+
+                            <li class="nav-items">
+
+                                <a href="#">
+                                    About
+                                </a>
+
+                            </li>
+
+                            <li class="nav-items">
+
+                                <a href="#">
+                                    Services
+                                </a>
+
+                            </li>
+
+                            <li class="nav-items">
+
+                                <a href="#">
+                                    Projects
+                                </a>
+
+                            </li>
+
+                            <li class="nav-items">
+
+                                <a href="#">
+                                    Team
+                                </a>
+
+                            </li>
+
+                        </ul>
+
+                    </div>
+
+                </div>
+
+            </nav>
+
+            <div class="hero">
+
+            </div>
+
+        </header>
+
+    </body>
+
+</html>
